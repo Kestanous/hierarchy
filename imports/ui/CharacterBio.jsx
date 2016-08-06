4d2cb24990608e1f199623615961de1053078b90
Sivli Kestanous <cmtoups@me.com>
2016-08-06 16:23:46 -0400

    name and bio is now editable

--- imports/ui/CharacterBio.jsx
+++ imports/ui/CharacterBio.jsx
@@ -4,25 +4,27 @@ import { createContainer } from 'meteor/react-meteor-data';
 
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
-
 import TextField from 'material-ui/TextField';
+
+import { Characters } from '../api/collections.jsx';
 
 export default class CharacterBio extends Component {
 
-	constructor() {
-		super()
-		this.state = {edit: false}
+	constructor(props) {
+		super(props)
+		this.state = {
+      edit: false,
+      editName: props.character.name,
+      editBio: props.character.bio
+    }
 	}
 
 	getTitle() {
 		let title = this.props.character.name
 		if (this.state.edit) {
-			title = (
-				<div>
-					<TextField value={this.props.character.name}
-                  floatingLabelText="Character Name" floatingLabelFixed={true}/>
-        </div>
-			)
+			title = <TextField value={this.state.editName} floatingLabelText="Character Name" floatingLabelFixed={true} onChange={(event) => {
+        this.setState({editName: event.target.value})
+      }}/>
 		}
 		return title
 	}
@@ -32,12 +34,10 @@ export default class CharacterBio extends Component {
 	}
 
 	getBioEdit() {
-		return (
-			<div>
-				<TextField value={this.props.character.bio}
-                  floatingLabelText="Character Bio" floatingLabelFixed={true} multiLine={true} rows={2}/>
-      </div>
-		)
+		return <TextField value={this.state.editBio} floatingLabelText="Character Bio"
+      floatingLabelFixed={true} multiLine={true} onChange={(event) => {
+        this.setState({editBio: event.target.value})
+      }}/>
 	}
 
   render() {
@@ -61,6 +61,15 @@ export default class CharacterBio extends Component {
   }
 
 	onEditToggle() {
+    if (this.state.edit) {
+      let name = this.state.editName, bio = this.state.editBio
+      Characters.update({_id: this.props.character._id}, {$set: {name, bio}})
+    } else {
+      this.setState({
+        editName: this.props.character.name,
+        editBio: this.props.character.bio
+      })
+    }
 		this.setState({edit: !this.state.edit})
 	}
 }
