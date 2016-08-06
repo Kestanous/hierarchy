d81b20436b138436bd5c61315a1098dfe6d2a839
Sivli Kestanous <cmtoups@me.com>
2016-08-06 14:04:23 -0400

    used a proper navebar

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -1,17 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 
-import AppBar from 'material-ui/AppBar';
+import Paper from 'material-ui/Paper';
 
 
 
 export default class NavBar extends Component {
   render() {
-  		return (
-		  <AppBar
-    title="Title"
-    iconClassNameRight="muidocs-icon-navigation-expand-more"
-  	/>
-	);
-}
+    style = {
+      height: 64,
+      width: '100%',
+      textAlign: 'center',
+      display: 'inline-block',
+      "background-color": 'rgb(0, 188, 212)'
+    };
+  	return (
+      <Paper style={style} zDepth={1}>
+
+      </Paper>);
+  }
 }
