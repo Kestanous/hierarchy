ed74613c8a1ca3f6e6b5bab3f115159c33930191
miadea <kitcat2675@gmail.com>
2016-08-07 12:29:32 -0400

    added buttons to nav bar

--- imports/ui/NavBar.jsx
+++ imports/ui/NavBar.jsx
@@ -2,6 +2,9 @@ import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 
 import Paper from 'material-ui/Paper';
+import FlatButton from 'material-ui/FlatButton';
+import HardwareKeyboardBackspace from 'material-ui/svg-icons/hardware/keyboard-backspace';
+import ActionPowerSettingsNew from 'material-ui/svg-icons/action/power-settings-new';
 
 
 
@@ -15,8 +18,30 @@ export default class NavBar extends Component {
       "backgroundColor": 'rgb(0, 188, 212)'
     };
   	return (
-      <Paper style={style} zDepth={1}>
-
+      <Paper className="navBar" style={style} zDepth={1}>
+        <div>
+          <FlatButton
+            style = {
+            {height: 64} }
+            label="Back"
+            labelPosition="after"
+            primary={false}
+            icon={<HardwareKeyboardBackspace />}
+          />
+        </div>
+        <h2 className="navBarTitle">
+          H e i r a r c h y
+        </h2>
+        <div>
+          <FlatButton
+            style = {
+            {height: 64} }
+            label="Log Out"
+            labelPosition="before"
+            primary={false}
+            icon={<ActionPowerSettingsNew />}
+          />
+        </div>
       </Paper>);
   }
 }
